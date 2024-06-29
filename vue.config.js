213c73be30e1265c35dc046a6ac90060d2e0cdd2
Moeary <Minecraftyyxx@gmail.com>
2024-06-29 16:27:23 +0800

    first update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false, //关闭语法检查
+  assetsDir: "static",
+  devServer: {
+    port: "8000",
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:80',
+        pathRewrite: {
+          '^/api': '/api'
+        },
+        changeOrigin: true,
+        ws: trueS
+      }
+    }
+  },
 })
