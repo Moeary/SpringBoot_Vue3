9a8a042e76c63a1f3da5b5354da19f00cb298fd7
Moeary <103913682+Moeary@users.noreply.github.com>
2024-07-04 10:53:32 +0800

    Update user.sql

--- sql/user.sql
+++ sql/user.sql
@@ -4,6 +4,3 @@ CREATE TABLE user (
                       password VARCHAR(255) NOT NULL,
                       is_admin BOOLEAN DEFAULT FALSE
 );
-INSERT INTO user (id_number, password, is_admin) VALUES
-     ('340521200304075217', '111111', TRUE), -- 管理员
-     ('420114200405172213', '111111', FALSE); -- 普通用户
